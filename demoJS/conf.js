1deee96a76a701c53c6142a82555eaf9583de7a4
amarhr <amarnathhr86@gmail.com>
2017-07-12 15:37:00 +0530

    Update conf.js

--- demoJS/conf.js
+++ demoJS/conf.js
@@ -1,8 +1,8 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  //specs: ['testspec.js']
-  specs: ['zoo_spec.js'],
+  specs: ['testspec.js'],
+  //specs: ['zoo_spec.js'],
   capabilities: {
     browserName: 'chrome'
   },
